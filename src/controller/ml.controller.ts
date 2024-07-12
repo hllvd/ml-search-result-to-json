077fcc61d0d924d4fec19b7c65b7f86f338c5d22
Hudson leandro Leal van-dal <hllvd@hotmail.com>
2024-07-12 05:51:30 -0300

    remove unused code

--- src/controller/ml.controller.ts
+++ src/controller/ml.controller.ts
@@ -1,10 +1,9 @@
 import { Request, Response } from "express"
-import { getAppConfig } from "../repository/app-config.repository"
 import { getMe } from "../services/ml/api/me"
 const me = async (req: Request, res: Response) => {
   const r = await getMe()
   console.log("r", r)
-  res.status(200).json({ message: "Hello", ...r })
+  res.status(200).json({ ...r })
 }
 
 const notification = async (req: Request, res: Response) => {}

0ca5ccb14b4271989ceb51889022ad4239aa313a
Hudson leandro Leal van-dal <hllvd@hotmail.com>
2024-08-04 19:00:41 -0300

    config jest && add unit test cases

--- src/services/ml/reducers/catalog.reducer.ts
+++ src/services/ml/reducers/catalog.reducer.ts
@@ -1,30 +1,45 @@
 import { LogisticType, MLProduct } from "../../../models/dto/ml-product.models"
 
 export const catalogReducer = (catalog: Array<MLProduct>) => {
-  catalog.reduce(
+  return catalog.reduce(
     (acc, curr, i) => {
       const isFull = curr.shipping?.logistic_type == LogisticType.full
       acc.firstPlace = i == 0 ? curr.price : acc.firstPlace
+
       acc.sumPrice += curr.price
-      acc.bestPrice = Math.min(acc.bestPrice, curr.price)
+
+      acc.bestPrice =
+        acc.bestPrice === null
+          ? curr.price
+          : Math.min(acc.bestPrice, curr.price)
+
       acc.secondBestPrice =
-        curr.price <= acc.bestPrice
+        acc.secondBestPrice === null && curr.price > acc.bestPrice
+          ? curr.price
+          : acc.secondBestPrice
+      acc.secondBestPrice =
+        curr.price > acc.bestPrice
           ? Math.min(acc.secondBestPrice, curr.price)
           : acc.secondBestPrice
+
+      acc.bestPriceFull =
+        isFull && acc.bestPriceFull === null ? curr.price : acc.bestPriceFull
       acc.bestPriceFull = isFull
-        ? Math.min(acc.secondBestPrice, curr.price)
+        ? Math.min(acc.bestPriceFull, curr.price)
         : acc.bestPriceFull
+
       acc.fullBestPosition =
         isFull && acc.fullBestPosition == null ? i : acc.fullBestPosition
+
       acc.length += 1
       return acc
     },
     {
       sumPrice: 0,
-      bestPrice: 0,
-      secondBestPrice: 0,
+      bestPrice: null,
+      secondBestPrice: null,
       firstPlace: 0,
-      bestPriceFull: 0,
+      bestPriceFull: null,
       fullBestPosition: null,
       length: 0,
     }

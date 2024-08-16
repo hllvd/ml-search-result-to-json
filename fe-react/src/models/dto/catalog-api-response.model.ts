2964b5a00875ca749b52ad4c6cf38c5abd60809c
Hudson leandro Leal van-dal <hllvd@hotmail.com>
2024-08-15 21:13:41 -0300

    update interfaces

--- fe-react/src/models/dto/catalog-api-response.model.ts
+++ fe-react/src/models/dto/catalog-api-response.model.ts
@@ -3,11 +3,45 @@ export interface CatalogInformationResponse {
   title: string
   ean: string
   brandModel: BrandModel
+  permalink: string
+  thumbnail: string
+  price: Price
+  bestPriceFull: number
+  position: Position
+  length: number
+  dateCreated: string
+  mlOwner: boolean
+  shipmentByState: ShipmentByState
+  medalByState: MedalByState
+  state: State
   revenue: number
   dailyRevenue: number
 }
-export interface BrandModel {
+
+interface State {
+  [key: string]: any
+}
+interface MedalByState {
+  [key: string]: any
+}
+
+interface MedalLider {
+  [key: string]: number
+}
+interface ShipmentByState {
+  [key: string]: any
+}
+
+interface Position {
+  [key: string]: number
+}
+interface Price {
+  top5Avg: number
+  best: number
+  secondBest: number
+}
+interface BrandModel {
   brand: string
-  model: string
-  color: string
+  model?: any
+  color?: any
 }

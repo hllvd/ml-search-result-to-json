3c2d974394ed7d319c180967de67799c0e503dbf
Hudson leandro Leal van-dal <hllvd@hotmail.com>
2024-08-25 15:33:36 -0300

    settings

--- fe-react/src/services/api/ml-catalog.api.service.ts
+++ fe-react/src/services/api/ml-catalog.api.service.ts
@@ -8,8 +8,9 @@ export const fetchCatalogInformation = async ({
   userId,
   catalogId,
 }: FetchCatalogParam): Promise<CatalogInformationResponse> => {
+  console.log(process.env.REACT_APP_ML_DATA)
   const res = await fetch(
-    `https://localhost:3333/ml/catalog?catalogId=${catalogId}&userId=${userId}`
+    `${process.env.REACT_APP_ML_DATA}/ml/catalog?catalogId=${catalogId}&userId=${userId}`
   )
   return res.json()
 }

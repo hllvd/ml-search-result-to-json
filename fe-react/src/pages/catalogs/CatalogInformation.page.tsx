ed5b0d0f97831b757f96492df1aa7cb514935414
Hudson leandro Leal van-dal <hllvd@hotmail.com>
2024-09-05 10:42:03 -0300

    fix loading

--- fe-react/src/pages/catalogs/CatalogInformation.page.tsx
+++ fe-react/src/pages/catalogs/CatalogInformation.page.tsx
@@ -5,7 +5,7 @@ import SearchCatalogComponent from "../../components/features/catalogs/SearchCat
 import { extractProductId } from "../../utils/Regex.util"
 import { useNavigate, useParams } from "react-router-dom"
 import CatalogInformationContent from "../../components/features/catalogs/CatalogInformationContent.component"
-
+import "./CatalogInformation.scss"
 const CatalogInformationPage: React.FC = () => {
   const { id } = useParams()
   const navigate = useNavigate()
@@ -30,7 +30,7 @@ const CatalogInformationPage: React.FC = () => {
         isLoading={isLoading}
       />
       {isError && <p>Error fetching data</p>}
-      {isLoading && searchTerm && <LoadingOutlined />}
+      {isLoading && searchTerm && <LoadingOutlined className="page-loading" />}
       {isFetched && searchTerm && (
         <CatalogInformationContent catalogData={data} productId={searchTerm} />
       )}

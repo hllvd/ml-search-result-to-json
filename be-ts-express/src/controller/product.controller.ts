e21ce2ba0913b2b4bd1ceeed3395a814c0dff1ae
Hudson leandro Leal van-dal <hllvd@hotmail.com>
2024-11-03 07:33:27 -0300

    handling error for product

--- be-ts-express/src/controller/product.controller.ts
+++ be-ts-express/src/controller/product.controller.ts
@@ -3,31 +3,39 @@ import { getProductVisitsSummary } from "../services/ml/products-visits.service"
 import { getProductComplete } from "../services/ml/products.service"
 
 const product = async (req: Request, res: Response) => {
-  const productId = req.query?.productId?.toString()
-  const userId = req.query?.userId?.toString() ?? "1231084821"
+  try {
+    const productId = req.query?.productId?.toString()
+    const userId = req.query?.userId?.toString() ?? "1231084821"
 
-  const productInfo = await getProductComplete({
-    productId,
-    userId,
-  })
+    const productInfo = await getProductComplete({
+      productId,
+      userId,
+    })
 
-  res.status(200).json({
-    productId,
-    ...productInfo,
-  })
+    res.status(200).json({
+      productId,
+      ...productInfo,
+    })
+  } catch (e) {
+    res.status(400).json({ message: e.message })
+  }
 }
 
 const views = async (req: Request, res: Response) => {
-  const productId = req.query?.productId?.toString()
-  const userId = req.query?.userId?.toString() ?? "1231084821"
+  try {
+    const productId = req.query?.productId?.toString()
+    const userId = req.query?.userId?.toString() ?? "1231084821"
 
-  const catalogVisitsSummary = await getProductVisitsSummary({
-    userId,
-    productId,
-  })
-  res.status(200).json({
-    ...catalogVisitsSummary,
-  })
+    const catalogVisitsSummary = await getProductVisitsSummary({
+      userId,
+      productId,
+    })
+    res.status(200).json({
+      ...catalogVisitsSummary,
+    })
+  } catch (e) {
+    res.status(400).json({ message: e.message })
+  }
 }
 
 export default { product, views }
